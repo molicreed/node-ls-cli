e7b785b96f8451bc7989c2ff0593ce3bf5fad87d
MoLi <2560263173@qq.com>
2017-07-18 01:00:20 +0800

    handle many filePath and parameter

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const fs = require('fs')
+const path = require('path')
+
 const [, , ...argv] = process.argv
+const PATH = new Set()
+const PARAMETER = new Set()
 
 const isHidden = (fileName) => {
     const regex = /^\./
@@ -9,25 +13,75 @@ const isHidden = (fileName) => {
         return false
     }
 }
-if (argv.length == 0) {
-    try {
-        let files = fs.readdirSync(process.cwd())
-        for (let fileName of files) {
-            if ( !isHidden(fileName)) {
 
-                let stats = fs.statSync(fileName)
-
-                if (stats.isDirectory()) {
-                    process.stdout.write(fileName + '/  ')
-                    // console.log(fileName+ '/  ')
-                } else if (stats.isFile()) {
-                    process.stdout.write(fileName + '  ')
-                    // console.log(fileName + '  ')
-                }
-            }
-
-        }
-    } catch (err) {
-        console.error(err)
+const isParameter = (text) => {
+    const regex = /^\-/
+    if (regex.test(text)){
+        return true
+    } else {
+        return false
     }
 }
+
+const spliteParameter = (text) =>{
+    if (text[0] == '-'){
+        let paraArr = text.split('')
+        paraArr.shift()
+        return paraArr
+    } else {
+        throw new Error('arguments is not parameter')
+    }
+}
+
+
+
+if (argv.length == 0){
+    PATH.add('.')
+} else {
+    for (let text of argv){
+        if (isParameter(text)){
+            PARAMETER.add(...spliteParameter(text))
+        } else {
+            PATH.add(text)
+        }
+    }
+}
+
+for (let pathString of PATH){
+    process.stdout.write(pathString+':\n')
+    try {
+        if (fs.existsSync(pathString)){
+            //file or directory is exists
+            let stats = fs.statSync(pathString)
+
+            if (stats.isFile()){
+                process.stdout.write(pathString + '  ')
+
+            } else if (stats.isDirectory()){
+                
+                let files = fs.readdirSync(pathString)
+                for (let fileName of files) {
+                    if ( !isHidden(fileName)) {
+
+                        let stats = fs.statSync(fileName)
+
+                        if (stats.isDirectory()) {
+                            process.stdout.write(fileName + '/  ')
+                            // console.log(fileName+ '/  ')
+                        } else if (stats.isFile()) {
+                            process.stdout.write(fileName + '  ')
+                            // console.log(fileName + '  ')
+                        }
+                    }
+
+                }
+            }
+        } else {
+            throw new Error('node-ls-cli: cannot access '+ pathString + ': No such file or dircetory')
+        }
+    } catch (err) {
+        console.error(err.message)
+    }
+    process.stdout.write('\n')
+}
+
