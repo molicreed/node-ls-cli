d13abe72c78282602265d5571833518e1d440022
MoLi <2560263173@qq.com>
2017-07-17 22:29:53 +0800

    use fs to read files

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
+const fs = require('fs')
+const [ , ,...argv] = process.argv
 
-let [...argv] = process.argv
-
-console.log(argv)
+if (argv.length == 0 ){
+    fs.readdir(process.cwd(), (err, files)=>{
+        if (err){
+            console.error(err)
+            return
+        }
+        for (let fileName of files){
+            console.log(fileName)
+        }
+        
+    })
+}
