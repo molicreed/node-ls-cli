76ee5b18b9d79e613d2387674e15f75d6f84e8a9
MoLi <2560263173@qq.com>
2017-07-18 00:02:53 +0800

    hidden files with .*

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const fs = require('fs')
-const [ , ,...argv] = process.argv
+const [, , ...argv] = process.argv
 
-if (argv.length == 0 ){
+const isHidden = (fileName) => {
+    const regex = /^\./
+    if (regex.test(fileName)) {
+        return true
+    } else {
+        return false
+    }
+}
+if (argv.length == 0) {
     try {
         let files = fs.readdirSync(process.cwd())
-        for (let fileName of files){
-            let stats = fs.statSync(fileName)
-            if (stats.isDirectory()){
-                process.stdout.write(fileName+ '/  ')
-                // console.log(fileName+ '/  ')
-            } else if (stats.isFile()){
-                process.stdout.write(fileName+ '  ')
-                // console.log(fileName + '  ')
+        for (let fileName of files) {
+            if ( !isHidden(fileName)) {
+
+                let stats = fs.statSync(fileName)
+
+                if (stats.isDirectory()) {
+                    process.stdout.write(fileName + '/  ')
+                    // console.log(fileName+ '/  ')
+                } else if (stats.isFile()) {
+                    process.stdout.write(fileName + '  ')
+                    // console.log(fileName + '  ')
+                }
             }
+
         }
     } catch (err) {
         console.error(err)
