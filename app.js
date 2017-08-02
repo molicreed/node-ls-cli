188b452ad2bdda32400dfe312fa8d4df97e69022
Coffee-Mac <coffee_limo@outlook.com>
2017-08-02 11:17:26 +0800

    change add traverse and print

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const fs = require('fs')
 const path = require('path')
+
 const handleArgv = require('./util/handle-argv')
-const iteratorDircetory = require('./util/iterator-directory')
 const FileInfo = require('./util/file-info')
 const [, , ...argv] = process.argv
 
 const { PATH, PARAMETER } = handleArgv(argv)
 
 let pathLen = PATH.size
-
-
 
 for (let pathString of PATH) {
     if (pathLen >= 2) {
@@ -21,18 +19,12 @@ for (let pathString of PATH) {
             //file or directory is exists
 
             let stats = fs.statSync(absolutePath)
-            let fileInfo = new FileInfo({ absolutePath, stats})
-            if (fileInfo.type == 'file'){
-                process.stdout.write(fileInfo.name + '  ')
+            let fileInfo = new FileInfo({ absolutePath})
+            if (!fileInfo.isDir){
+                file.print()
             } else {
-                let fileInfoArr = iteratorDircetory(fileInfo)
-                for (let file of fileInfoArr){
-                    process.stdout.write(file.name)
-                    if (file.type == 'dir'){
-                        process.stdout.write('/  ')
-                    } else {
-                        process.stdout.write('  ')
-                    }
+                for (let file of fileInfo.traverse()){
+                    file.print()
                 }
             }
         }
--- util/file-info.js
+++ util/file-info.js
@@ -1,14 +1,43 @@
 
+const fs = require('fs')
 const path = require('path')
 const isHidden = require('../api/is-hidden')
+const iteratorDircetory = require('./iterator-directory')
 
 class FileInfo {
-    constructor({absolutePath, stats}) {
+    constructor({ absolutePath }) {
+        let stats = fs.statSync(absolutePath)
         this.name = path.parse(absolutePath).base
         this.path = absolutePath
-        this.type = stats.isFile() ? 'file' : stats.isDirectory() ? 'dir' : 'unknow'
+        this.isDir = stats.isDirectory()
         this.birthTime = stats.birthtime
         this.hidden = isHidden(this.name)
+        this.files = []
+    }
+
+    print() {
+        process.stdout.write(this.name)
+        if (this.isDir) process.stdout.write('/')
+        process.stdout.write('  ')
+    }
+
+    traverse() {
+        if (this.isDir) {
+            let files = fs.readdirSync(this.path)
+            for (let fileName of files) {
+                try {
+                    let absolutePath = path.resolve(this.path, fileName)
+                    let stats = fs.statSync(absolutePath)
+                    this.files.push(new this.constructor({ absolutePath }))
+                } catch (err) {
+                    if (err.message.match('operation not permitted')) {
+
+                    }
+
+                }
+            }
+        }
+        return this.files
     }
 }
 
